202b80791f445b1e9fa9a740a26b1faa920de250
Selva <47226341+selvadhurai@users.noreply.github.com>
2023-09-12 14:03:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var http = require('http');
+// Import the required HTTP module
+const http = require('http');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Selva node js testing in the docker images'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 80
+// Define the hostname and port for the server
+const hostname = '0.0.0.0'; // Listen on all network interfaces
+const port = 8080; // Use port 8080
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // HTTP status code 200 (OK)
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n'); // Response text
+});
+
+// Start the server and listen on the specified hostname and port
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
